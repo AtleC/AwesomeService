f49f6dbfdf5c07aa357436474428f619d650cbd5
Atle Christoffer Corneliussen <atle.corneliussen@intility.no>
2021-07-26 09:42:02 +0200

    IPC between AwsomeConsole and AwesomeConsole2

---
+++ AwesomeGUI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Windows;
+
+namespace AwesomeGUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
